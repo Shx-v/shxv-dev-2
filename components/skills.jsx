d690e572ce5e2f4adeddf81ca0315e25c8c9aaf3
shiv <shivranjanbharadwaj@gmail.com>
2024-03-16 21:01:18 +0530

    changed

--- components/skills.jsx
+++ components/skills.jsx
@@ -12,7 +12,7 @@ import {
         className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-80 py-20"
         style={{ transform: "scale(0.9" }}
       >
-        {/* <SkillText /> */}
+        <SkillText />
   
         <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
           {Frontend_skill.map((image, index) => (
